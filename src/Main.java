9dc83421349898af12bb5d886b167b0a6519a6a1
yulialener <lener.yu@yandex.ru>
2024-06-16 12:14:11 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int initialSum = 100;
+        int addedSum = 1100;
+        int finalSum;
+        int bonus;
+        if (addedSum > 1000) {
+            bonus = addedSum / 100;
+            finalSum = initialSum + addedSum + bonus;
+            System.out.println("Итоговый счет = " + finalSum);
+            System.out.println("Бонус = " + bonus);
+        } else {
+            finalSum = initialSum + addedSum;
+            bonus = 0;
+            System.out.println("Итоговый счет = " + finalSum);
+            System.out.println("Бонус = " + bonus);
+        }
+    }
+}
